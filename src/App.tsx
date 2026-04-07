188bf455313ec4820367eb6e421ba5b0c4ef7ee7
sun3350 <isaacayomide2359@gmail.com>
2026-04-07 14:33:40 +0100

    update

--- src/App.tsx
+++ src/App.tsx
@@ -39,7 +39,7 @@ const App: React.FC = () => {
             path="/"
             element={
               <Layout>
-                <ComingSoonPage />
+                <Home />
               </Layout>
             }
           />
