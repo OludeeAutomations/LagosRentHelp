fb07c0f3491479b287715bcb6eaf189866e97faa
sun3350 <isaacayomide2359@gmail.com>
2026-04-09 09:07:19 +0100

    update property

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,10 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  useLocation,
+} from "react-router-dom";
 import { AnimatePresence } from "framer-motion";
 import Layout from "./layout";
 import Home from "./page/home/Home";
@@ -26,194 +31,205 @@ import PropertyManagementPage from "./page/admin/PropertyManagementPage";
 import PropertyEditorPage from "./page/admin/PropertyEditorPage";
 import AdminAccountsPage from "./page/admin/AdminAccountsPage";
 
-const App: React.FC = () => {
+const AppContent: React.FC = () => {
+  const location = useLocation();
   return (
-    <Router>
+    <>
       <Toaster position="top-right" />
 
       <AnimatePresence mode="wait">
-        <ScrollToTop />
-        <Routes>
-          {/* Public Routes with Main Layout */}
-          <Route
-            path="/"
-            element={
-              <Layout>
-                <Home />
-              </Layout>
-            }
-          />
-          <Route
-            path="/contact"
-            element={
-              <Layout>
-                <Contact />
-              </Layout>
-            }
-          />
-          <Route
-            path="/about"
-            element={
-              <Layout>
-                <About />
-              </Layout>
-            }
-          />
-          <Route
-            path="/search"
-            element={
-              <Layout>
-                <SearchPage />
-              </Layout>
-            }
-          />
-          <Route
-            path="/settings"
-            element={
-              <Layout>
-                <UserSettingsPage />
-              </Layout>
-            }
-          />
-          <Route
-            path="/dashboard/favorites"
-            element={
-              <Layout>
-                <Favorites />
-              </Layout>
-            }
-          />
-          <Route
-            path="/features"
-            element={
-              <Layout>
-                <Features />
-              </Layout>
-            }
-          />
-          <Route
-            path="/privacy"
-            element={
-              <Layout>
-                <PrivacyPolicy />
-              </Layout>
-            }
-          />
-          <Route
-            path="/terms"
-            element={
-              <Layout>
-                <TermsOfService />
-              </Layout>
-            }
-          />
-          <Route
-            path="/faq"
-            element={
-              <Layout>
-                <FAQPage />
-              </Layout>
-            }
-          />
-          <Route
-            path="/register"
-            element={
-              <Layout>
-                <Register />
-              </Layout>
-            }
-          />
-          <Route
-            path="/login"
-            element={
-              <Layout>
-                <Login />
-              </Layout>
-            }
-          />
-          <Route
-            path="/verify-email"
-            element={
-              <Layout>
-                <EmailVerificationNotice />
-              </Layout>
-            }
-          />
-          <Route
-            path="/verify-email/:userId/:token"
-            element={
-              <Layout>
-                <VerifyEmailResult />
-              </Layout>
-            }
-          />
-          <Route
-            path="/forgot-password"
-            element={
-              <Layout>
-                <ForgotPassword />
-              </Layout>
-            }
-          />
-          <Route
-            path="/reset-password/:userId/:token"
-            element={
-              <Layout>
-                <ResetPassword />
-              </Layout>
-            }
-          />
-          <Route
-            path="/properties/:id"
-            element={
-              <Layout>
-                <PropertyDetails />
-              </Layout>
-            }
-          />
-          <Route
-            path="/admin/properties"
-            element={
-              <Layout>
-                <PropertyManagementPage />
-              </Layout>
-            }
-          />
-          <Route
-            path="/admin/properties/new"
-            element={
-              <Layout>
-                <PropertyEditorPage />
-              </Layout>
-            }
-          />
-          <Route
-            path="/admin/properties/:id/edit"
-            element={
-              <Layout>
-                <PropertyEditorPage />
-              </Layout>
-            }
-          />
-          <Route
-            path="/admin/accounts"
-            element={
-              <Layout>
-                <AdminAccountsPage />
-              </Layout>
-            }
-          />
-          {/* 404 Page - Keep this at the end */}
-          <Route
-            path="*"
-            element={
-              <Layout>
-                <ComingSoonPage />
-              </Layout>
-            }
-          />
-        </Routes>
+        <ScrollToTop key="scroll-to-top" />
+        <div key={location.pathname}>
+          <Routes>
+            {/* Public Routes with Main Layout */}
+            <Route
+              path="/"
+              element={
+                <Layout>
+                  <Home />
+                </Layout>
+              }
+            />
+            <Route
+              path="/contact"
+              element={
+                <Layout>
+                  <Contact />
+                </Layout>
+              }
+            />
+            <Route
+              path="/about"
+              element={
+                <Layout>
+                  <About />
+                </Layout>
+              }
+            />
+            <Route
+              path="/search"
+              element={
+                <Layout>
+                  <SearchPage />
+                </Layout>
+              }
+            />
+            <Route
+              path="/settings"
+              element={
+                <Layout>
+                  <UserSettingsPage />
+                </Layout>
+              }
+            />
+            <Route
+              path="/dashboard/favorites"
+              element={
+                <Layout>
+                  <Favorites />
+                </Layout>
+              }
+            />
+            <Route
+              path="/features"
+              element={
+                <Layout>
+                  <Features />
+                </Layout>
+              }
+            />
+            <Route
+              path="/privacy"
+              element={
+                <Layout>
+                  <PrivacyPolicy />
+                </Layout>
+              }
+            />
+            <Route
+              path="/terms"
+              element={
+                <Layout>
+                  <TermsOfService />
+                </Layout>
+              }
+            />
+            <Route
+              path="/faq"
+              element={
+                <Layout>
+                  <FAQPage />
+                </Layout>
+              }
+            />
+            <Route
+              path="/register"
+              element={
+                <Layout>
+                  <Register />
+                </Layout>
+              }
+            />
+            <Route
+              path="/login"
+              element={
+                <Layout>
+                  <Login />
+                </Layout>
+              }
+            />
+            <Route
+              path="/verify-email"
+              element={
+                <Layout>
+                  <EmailVerificationNotice />
+                </Layout>
+              }
+            />
+            <Route
+              path="/verify-email/:userId/:token"
+              element={
+                <Layout>
+                  <VerifyEmailResult />
+                </Layout>
+              }
+            />
+            <Route
+              path="/forgot-password"
+              element={
+                <Layout>
+                  <ForgotPassword />
+                </Layout>
+              }
+            />
+            <Route
+              path="/reset-password/:userId/:token"
+              element={
+                <Layout>
+                  <ResetPassword />
+                </Layout>
+              }
+            />
+            <Route
+              path="/properties/:id"
+              element={
+                <Layout>
+                  <PropertyDetails />
+                </Layout>
+              }
+            />
+            <Route
+              path="/admin/properties"
+              element={
+                <Layout>
+                  <PropertyManagementPage />
+                </Layout>
+              }
+            />
+            <Route
+              path="/admin/properties/new"
+              element={
+                <Layout>
+                  <PropertyEditorPage />
+                </Layout>
+              }
+            />
+            <Route
+              path="/admin/properties/:id/edit"
+              element={
+                <Layout>
+                  <PropertyEditorPage />
+                </Layout>
+              }
+            />
+            <Route
+              path="/admin/accounts"
+              element={
+                <Layout>
+                  <AdminAccountsPage />
+                </Layout>
+              }
+            />
+            {/* 404 Page - Keep this at the end */}
+            <Route
+              path="*"
+              element={
+                <Layout>
+                  <ComingSoonPage />
+                </Layout>
+              }
+            />
+          </Routes>
+        </div>
       </AnimatePresence>
+    </>
+  );
+};
+
+const App: React.FC = () => {
+  return (
+    <Router>
+      <AppContent />
     </Router>
   );
 };
